1cff736b207b3bb02c1543a4e4a26352d596efed
Sami <sami79031@yahoo.com>
2020-10-08 18:40:15 +0300

    Refactoring `CoreDataFeedStore` to its own file

---
+++ FeedStoreChallenge/CoreDataStore/CoreDataFeedStore.swift
@@ -0,0 +1,48 @@
+//
+//  CoreDataFeedStore.swift
+//  FeedStoreChallenge
+//
+//  Created by Sami Ali on 10/8/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class CoreDataFeedStore: FeedStore {
+    
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+    
+    public init(storeURL: URL, bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(withModelName: "Model", url: storeURL, in: bundle)
+        context = container.newBackgroundContext()
+    }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        fatalError("Not omplemented")
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        let cache = Cache(context: context)
+        cache.timestamp = timestamp
+        cache.managedFeeds = feed.mapToManagedFeedImages(in: context).toNSOrderedSet
+        
+        try! context.save()
+        completion(nil)
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        guard let managedCaches = try? context.fetch(Cache.fetchRequest() as NSFetchRequest<Cache>),
+              let firstObject = managedCaches.first,
+              let timestamp = firstObject.timestamp else {
+            
+            return completion(.empty)
+        }
+        
+        let localFeedImages = firstObject.managedFeedImages.map({$0.localFeedImage})
+        completion(.found(feed: localFeedImages, timestamp: timestamp))
+    }
+    
+    
+}
